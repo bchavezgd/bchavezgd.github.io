cecaf03d37ff9ef677faef5fa64d714fffc065b2
bchavezgd <bchavez.gd@gmail.com>
2014-10-20 14:08:02 -0700

    prototype

---
+++ prototype/js/main.js
@@ -0,0 +1,14 @@
+/*
+ * Auto-generated content from the Brackets New Project extension.
+ */
+
+/*jslint vars: true, plusplus: true, devel: true, nomen: true, indent: 4, maxerr: 50 */
+/*global $, window, document */
+
+// Simple jQuery event handler
+
+// "use strict";
+jQuery(".col-db").click(function () {
+//	alert("Fuck off!");
+	jQuery("#fuckoff").hide();
+});
